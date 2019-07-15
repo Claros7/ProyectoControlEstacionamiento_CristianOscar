51e1a881bbf4c9b3b9887823d2b9a63eb154d9ac
Osckar07 <50966035+Osckar07@users.noreply.github.com>
2019-07-14 23:13:06 -0600

    Creación de ClaseEstacionamiento llenando los campos de la tabla

---
+++ ProyectoControlEstacionamiento_CristianOscar/ClaseEstacionamiento.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+
+
+namespace ProyectoControlEstacionamiento_CristianOscar
+{
+    class ClaseEstacionamiento
+    {
+        private DateTime horaEntrada;
+        private DateTime horaSalida;
+        private string placaVehiculo;
+        private string tipoVehiculo;
+        private decimal costo;
+        private int tiempoTotal;
+
+        SqlConnection cn = new SqlConnection("Data Source=OSCKAR_BENITES\\SQLEXPRESS;Initial Catalog=Estacionamiento;Integrated Security=True");
+        public ClaseEstacionamiento()
+        {
+            placaVehiculo = "PorDefecto";
+            tipoVehiculo = "PorDefecto";
+        }
+
+
+        public string PlacaVehiculo
+        {
+            get { return placaVehiculo; }
+            set { placaVehiculo = value; }
+        }
+
+        public string TipoVehiculo
+        {
+            get { return tipoVehiculo; }
+            set { tipoVehiculo = value; }
+        }
+
+        public DateTime HoraEntrada
+        {
+            get { return horaEntrada; }
+            set { horaEntrada = value; }
+        }
+
+        public DateTime HoraSalida
+        {
+            get { return horaSalida; }
+            set { horaSalida = value; }
+        }
+
+        public decimal Costo
+        {
+            get { return costo; }
+            set { costo = value; }
+        }
+
+        public int TiempoTotal
+        {
+            get { return tiempoTotal; }
+            set { tiempoTotal = value; }
+        }
+
+
+
--- ProyectoControlEstacionamiento_CristianOscar/MainWindow.xaml.cs
+++ ProyectoControlEstacionamiento_CristianOscar/MainWindow.xaml.cs
@@ -12,10 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
-//Bibliotecas para SQL
-using System.Data;
+using System.Configuration;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace ProyectoControlEstacionamiento_CristianOscar
 {
@@ -28,11 +27,43 @@ namespace ProyectoControlEstacionamiento_CristianOscar
         public MainWindow()
         {
             InitializeComponent();
+            this.lbReporteVehiculo.ItemsSource = parqueo.MostrarEntrada();
+            txtPlaca.Focus();
         }
 
-        private void Dtgrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void Salir(object sender, RoutedEventArgs e)
         {
-
+            Application.Current.Shutdown();
         }
+        private void Aceptar(object sender, RoutedEventArgs e)
+        {
+            ClaseEstacionamiento parqueo = new ClaseEstacionamiento();
+            if (txtPlaca.Text.Equals("") == false && cmbTipoVehiculo.SelectedIndex != -1)
+            {
+
+                parqueo.PlacaVehiculo = txtPlaca.Text;
+                parqueo.TipoVehiculo = cmbTipoVehiculo.Text;
+                parqueo.InsertarVehiculo();
+                txtPlaca.Clear();
+                cmbTipoVehiculo.SelectedIndex = -1;
+                txtPlaca.Focus();
+                this.lbReporteVehiculo.ItemsSource = parqueo.MostrarEntrada();
+            }
+            else
+            {
+                MessageBox.Show("Ingrese todos los datos para continuar");
+                txtPlaca.Clear();
+                cmbTipoVehiculo.SelectedIndex = -1;
+                txtPlaca.Focus();
+            }
+        }
+
+        private void Cancelar(object sender, RoutedEventArgs e)
+        {
+            txtPlaca.Clear();
+            cmbTipoVehiculo.SelectedIndex = -1;
+            txtPlaca.Focus();
+        }
+    }
     }
 }
